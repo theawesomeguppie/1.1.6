0ef98b4d7e1ce00af048c6b7031eaf27f4f32a09
HashDaMash <85582783+HashDaMash@users.noreply.github.com>
2026-09-08 12:36:42 -0700

    Update TheRiddler.java
    Performing operations on original number

--- TheRiddler.java
+++ TheRiddler.java
@@ -1,3 +1,20 @@
 public class TheRiddler{
-    
+    public static void main(String[] args) {    
+        int num = 12;
+        int newNum = num;
+        System.out.println(newNum);
+
+        newNum *= 2;
+        System.out.println(newNum);
+
+        newNum += 6;
+        System.out.println(newNum);
+
+        newNum /= 2;
+        System.out.println(newNum);
+
+        newNum -= num;
+        System.out.println(newNum);
+
+    }
 }
