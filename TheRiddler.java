cf665cb978056a581bee0192927d65e85fbd3aed
bhavyad09 <157430117+bhavyad09@users.noreply.github.com>
2026-09-10 11:01:22 -0700

    Update TheRiddler.java
    Added description of team contributions as a comment

--- TheRiddler.java
+++ TheRiddler.java
@@ -1,5 +1,15 @@
 public class TheRiddler{
-    public static void main(String[] args) {    
+    public static void main(String[] args) {  
+
+        /* The main code (below) was a collaborative effort
+        Other contributions:
+        Negative double test case: Ira
+        Negative integer test case: Trisha
+        One test case: Bhavya
+        Zero test case: Bhavya
+        Positive double test case: Ira
+        Positive integer test case: Hashim*/
+        
         int num = 12;
         int newNum = num;
         System.out.println(newNum);
